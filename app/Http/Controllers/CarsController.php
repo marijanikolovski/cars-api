8620558891a75adfdd67efd988eaa12ad35af38c
marijanikolovski <m.kuzeljevic@gmail.com>
2022-11-16 22:35:13 +0100

    task 5-Search by brand & model

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -16,7 +16,12 @@ class CarsController extends Controller
     public function index(Request $request)
     {
         $per_page = $request->query('per_page', 5);
-        $cars = Car::paginate($per_page);
+        $brand = $request->query('brand', '');
+        $model = $request->query('model', '');
+
+        $cars = Car::searchByBrand($brand)
+            ->searchByModel($model)
+            ->paginate($per_page);
 
         return $cars;
     }
--- app/Models/Car.php
+++ app/Models/Car.php
@@ -10,4 +10,20 @@ class Car extends Model
     use HasFactory;
     protected $table = 'cars';
     protected $fillable = ['brand', 'model', 'year', 'max_speed', 'is_automatic', 'engine', 'number_of_doors'];
+
+    public static function scopeSearchByBrand($query, $brand)
+    {
+        if (!$brand) {
+            return $query;
+        }
+        return self::where('brand', $brand);
+    }
+
+    public static function scopeSearchByModel($query, $model)
+    {
+        if (!$model) {
+            return $query;
+        }
+        return self::where('model', $model);
+    }
 }
